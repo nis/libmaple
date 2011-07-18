3b440784167fd2b4a5b2f74bbedcdfebff4b3bc5
Nis Sarup <nis@sarup.dk>
2011-07-18 13:28:12 +0200

    [IRLib] Started on the class.

--- libraries/IRLib/IRLib.h
+++ libraries/IRLib/IRLib.h
@@ -24,14 +24,47 @@
  * SOFTWARE.
  *****************************************************************************/
 
+#ifndef _IRLIB_H_
+#define _IRLIB_H_
+
 /***************************** Include files *******************************/
 
+#include "wirish.h"
+
 /*****************************    Defines    *******************************/
+
+// IR commands
+#define IRLIB_NO_COMMAND		0 	// No valid command received
+// Apple remote commands
+#define IRLIB_APPLE_UP			1
+#define IRLIB_APPLE_DOWN		2
+#define IRLIB_APPLE_LEFT		3
+#define IRLIB_APPLE_RIGHT		4
+#define IRLIB_APPLE_CENTER		5
+#define IRLIB_APPLE_MENU		6
+#define IRLIB_APPLE_PLAY		7
 
 /*****************************   Constants   *******************************/
 
 /*****************************   Variables   *******************************/
 
-/*****************************   Functions   *******************************/
+/*****************************     Class     *******************************/
+
+class IRLib {
+	public:
+		void irlib_init(uint8 ir_input_pin); 		// Setup.
+		void irlib_interrupt_handler(void); 		// Statemachine for handling incomming data.
+		
+		uint8 irlib_decode_apple(void);				// Decodes and returns the last received command.
+													// Does not delete command if the command is being repeated.
+													
+		
+	private:
+		uint8 raw_data_ready(void);					// Have we finished receiving a command?
+		uint16 command_repeating(void);				// Returns the number if times the current command has been repeated?
+		
+};
 
 /****************************** End Of Module *******************************/
+
+#endif
